cc51fecbd8a4e4186e411bb54946f450ddbbe070
iceice54 <125724815+iceice54@users.noreply.github.com>
2024-02-17 02:09:54 +0800

    Add eventlistener for each grid that toggles background colour to black when hovered

--- scripts.js
+++ scripts.js
@@ -4,5 +4,15 @@ for (let i = 0; i < (16 * 16); i++) {
     const div = document.createElement("div");
     div.classList.toggle("grid");
 
+    div.addEventListener("mouseover", () => {
+        div.classList.toggle("hovered");
+        console.log("hovered")
+    })
+
+    div.addEventListener("mouseout", () => {
+        div.classList.toggle("hovered");
+        console.log("unhovered")
+    })
+
     gridContainer.appendChild(div);
 }
